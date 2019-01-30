04ccc2bde1eac99e9faa32c0debbf73614b69b3c
Jonas Keinholz <jonas@keinholz.com>
2019-01-31 00:35:22 +0100

    chore: storybook

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,10 @@ module.exports = function(api) {
   api.cache(true)
 
   return {
-    presets: ['@babel/preset-env', '@babel/preset-typescript']
+    presets: [
+      '@babel/preset-env',
+      '@babel/preset-react',
+      '@babel/preset-typescript'
+    ]
   }
 }
---
+++ packages/demo/.storybook/webpack.config.js
@@ -0,0 +1,11 @@
+module.exports = (baseConfig, env, config) => {
+  config.module.rules.push({
+    test: /\.(ts|tsx)$/,
+    loader: require.resolve('babel-loader'),
+    options: {
+      rootMode: 'upward'
+    }
+  })
+  config.resolve.extensions.push('.ts', '.tsx')
+  return config
+}
