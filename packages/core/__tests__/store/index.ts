c6de83c9f99fc24a4b481557fb4bfee17965bd67
Jonas Keinholz <jonas@keinholz.com>
2019-02-06 18:37:24 +0100

    refactor(core): regroup store tests

--- packages/core/__tests__/store/index.ts
+++ packages/core/__tests__/store/index.ts
@@ -17,111 +17,117 @@ beforeEach(() => {
   }
 })
 
-test('insert', () => {
-  state = reducer(state, {
-    type: ActionType.Insert,
-    payload: {
-      id: '0',
-      plugin: 'stateless'
-    }
+describe('Insert', () => {
+  test('default plugin', () => {
+    state = reducer(state, {
+      type: ActionType.Insert,
+      payload: {
+        id: '0'
+      }
+    })
+    expect(getDocument(state, '0')).toEqual({ plugin: 'default' })
   })
-  expect(getDocument(state, '0')).toEqual({ plugin: 'stateless' })
-})
 
-test('insert default plugin', () => {
-  state = reducer(state, {
-    type: ActionType.Insert,
-    payload: {
-      id: '0'
-    }
+  test('stateless plugin', () => {
+    state = reducer(state, {
+      type: ActionType.Insert,
+      payload: {
+        id: '0',
+        plugin: 'stateless'
+      }
+    })
+    expect(getDocument(state, '0')).toEqual({ plugin: 'stateless' })
   })
-  expect(getDocument(state, '0')).toEqual({ plugin: 'default' })
-})
 
-test('insert stateful plugin', () => {
-  state = reducer(state, {
-    type: ActionType.Insert,
-    payload: {
-      id: '0',
-      plugin: 'stateful'
-    }
+  test('stateful plugin', () => {
+    state = reducer(state, {
+      type: ActionType.Insert,
+      payload: {
+        id: '0',
+        plugin: 'stateful'
+      }
+    })
+    expect(getDocument(state, '0')).toEqual({
+      plugin: 'stateful',
+      state: {
+        counter: 0
+      }
+    })
   })
-  expect(getDocument(state, '0')).toEqual({
-    plugin: 'stateful',
-    state: {
-      counter: 0
+})
+
+describe('remove', () => {
+  test('one document', () => {
+    state = {
+      ...state,
+      documents: { '0': { plugin: 'stateless' } }
     }
+    state = reducer(state, { type: ActionType.Remove, payload: '0' })
+    expect(getDocuments(state)).toEqual({})
+  })
+
+  test('two documents', () => {
+    state = {
+      ...state,
+      documents: { '0': { plugin: 'text' }, '1': { plugin: 'stateless' } }
+    }
+    state = reducer(state, { type: ActionType.Remove, payload: '0' })
+    expect(getDocuments(state)).toEqual({ '1': { plugin: 'stateless' } })
+  })
+
+  test('non-existing document', () => {
+    state = reducer(state, { type: ActionType.Remove, payload: '0' })
+    expect(getDocuments(state)).toEqual({})
   })
 })
 
-test('remove', () => {
-  state = {
-    ...state,
-    documents: { '0': { plugin: 'stateless' } }
-  }
-  state = reducer(state, { type: ActionType.Remove, payload: '0' })
-  expect(getDocuments(state)).toEqual({})
-})
-
-test('remove one', () => {
-  state = {
-    ...state,
-    documents: { '0': { plugin: 'text' }, '1': { plugin: 'stateless' } }
-  }
-  state = reducer(state, { type: ActionType.Remove, payload: '0' })
-  expect(getDocuments(state)).toEqual({ '1': { plugin: 'stateless' } })
-})
-
-test('remove not existing plugin', () => {
-  state = reducer(state, { type: ActionType.Remove, payload: '0' })
-  expect(getDocuments(state)).toEqual({})
-})
-
-test('change', () => {
-  state = {
-    ...state,
-    documents: { '0': { plugin: 'stateful' } }
-  }
-  state = reducer(state, {
-    type: ActionType.Change,
-    payload: {
-      id: '0',
+describe('change', () => {
+  test('whole state', () => {
+    state = {
+      ...state,
+      documents: { '0': { plugin: 'stateful' } }
+    }
+    state = reducer(state, {
+      type: ActionType.Change,
+      payload: {
+        id: '0',
+        state: { counter: 1 }
+      }
+    })
+    expect(getDocument(state, '0')).toEqual({
+      plugin: 'stateful',
       state: { counter: 1 }
-    }
+    })
   })
-  expect(getDocument(state, '0')).toEqual({
-    plugin: 'stateful',
-    state: { counter: 1 }
-  })
-})
 
-test('shallow change', () => {
-  state = {
-    ...state,
-    documents: {
-      '0': { plugin: 'stateful', state: { counter: 0, bar: 'bar' } }
+  test('shallow change', () => {
+    state = {
+      ...state,
+      documents: {
+        '0': { plugin: 'stateful', state: { counter: 0, bar: 'bar' } }
+      }
     }
-  }
-  state = reducer(state, {
-    type: ActionType.Change,
-    payload: {
-      id: '0',
-      state: { counter: 1 }
-    }
+    state = reducer(state, {
+      type: ActionType.Change,
+      payload: {
+        id: '0',
+        state: { counter: 1 }
+      }
+    })
+    expect(getDocument(state, '0')).toEqual({
+      plugin: 'stateful',
+      state: { counter: 1, bar: 'bar' }
+    })
   })
-  expect(getDocument(state, '0')).toEqual({
-    plugin: 'stateful',
-    state: { counter: 1, bar: 'bar' }
-  })
-})
 
-test('try changing missing plugin', () => {
-  state = reducer(state, {
-    type: ActionType.Change,
-    payload: {
-      id: '0',
-      state: { counter: 0 }
-    }
+  test('non-existing plugin', () => {
+    state = reducer(state, {
+      type: ActionType.Change,
+      payload: {
+        id: '0',
+        state: { counter: 0 }
+      }
+    })
+    expect(getDocuments(state)).toEqual({})
   })
-  expect(getDocuments(state)).toEqual({})
 })
