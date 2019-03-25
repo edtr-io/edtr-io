e689212350d5794076c94160ad612c4fc6f10341
Jonas Keinholz <git@inyono.dev>
2019-03-25 02:05:07 +0100

    chore: rebase onto master

--- packages/demo/__stories__/plugin-video.tsx
+++ packages/demo/__stories__/plugin-video.tsx
@@ -1,10 +1,11 @@
 import { storiesOf } from '@storybook/react'
 import * as React from 'react'
-import { Story } from '.'
 
-storiesOf('Video Plugin', module).add('Initial State', () => {
+import { EditorStory } from '../src'
+
+storiesOf('Plugins/Video', module).add('Initial State', () => {
   const state = {
     plugin: 'video'
   }
-  return <Story initialState={state} />
+  return <EditorStory initialState={state} />
 })
