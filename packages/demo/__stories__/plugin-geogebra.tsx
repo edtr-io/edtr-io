57c47972416b986790b92e6ce4fc42385aeaf607
Knorrke <knorr.b@gmx.de>
2019-03-28 17:28:41 +0100

    add prefilled stories for image, video and geogebra

--- packages/demo/__stories__/plugin-geogebra.tsx
+++ packages/demo/__stories__/plugin-geogebra.tsx
@@ -3,9 +3,17 @@ import * as React from 'react'
 
 import { EditorStory } from '../src'
 
-storiesOf('Plugins/GeoGebra', module).add('Initial State', () => {
-  const state = {
-    plugin: 'geogebra'
-  }
-  return <EditorStory initialState={state} />
-})
+storiesOf('Plugins/GeoGebra', module)
+  .add('Initial State', () => {
+    const state = {
+      plugin: 'geogebra'
+    }
+    return <EditorStory initialState={state} />
+  })
+  .add('Prefilled', () => {
+    const state = {
+      plugin: 'geogebra',
+      state: 'https://www.geogebra.org/m/Hfpaq7jQ'
+    }
+    return <EditorStory initialState={state} />
+  })
