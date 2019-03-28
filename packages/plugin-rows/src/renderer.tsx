df0f672e6ee9ecffb19671ebae8bc6fdbb691ba9
FridoSa <fridolin.sack@web.de>
2019-03-28 14:15:47 +0100

    hover for rows

--- packages/plugin-rows/src/renderer.tsx
+++ packages/plugin-rows/src/renderer.tsx
@@ -1,19 +1,15 @@
 import { StatefulPluginEditorProps } from '@edtr-io/core'
 import { rowsState } from '@edtr-io/plugin-rows'
 import * as React from 'react'
-import { styled } from '@edtr-io/ui'
-
-export const Row = styled.div({
-  margin: '5px 0'
-})
+import { Row } from './row'
 
 export const RowsRenderer = (
   props: StatefulPluginEditorProps<typeof rowsState>
 ) => {
   return (
     <React.Fragment>
-      {props.state().map(row => {
-        return <Row key={row.id}>{row.render()}</Row>
+      {props.state().map((row, index) => {
+        return <Row state={props.state} index={index} key={row.id} />
       })}
     </React.Fragment>
   )
