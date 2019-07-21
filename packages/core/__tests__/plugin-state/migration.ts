fd3708319085c6d39a81ff62bb913ad1bd68c16f
Jonas Keinholz <git@inyono.dev>
2019-07-21 15:46:05 +0200

    feat(core): add `StateType.migratable`

---
+++ packages/core/__tests__/plugin-state/migration.ts
@@ -0,0 +1,75 @@
+import { migratable, number, object } from '../../src/plugin-state'
+
+const deserializeHelpers = {
+  createDocument: () => {}
+}
+const serializeHelpers = {
+  getDocument: () => null
+}
+
+describe('migration', () => {
+  const state0 = number(0)
+  const state1 = object({
+    value: number(0)
+  })
+  const state2 = object({
+    value: number(0),
+    by: number(1)
+  })
+
+  const state = migratable(state0)
+    .migrate(state1, s => {
+      return { value: s }
+    })
+    .migrate(state2, s => {
+      return {
+        ...s,
+        by: 1
+      }
+    })
+
+  test('initial', () => {
+    expect(state.createInitialState(deserializeHelpers)).toEqual({
+      value: 0,
+      by: 1
+    })
+  })
+
+  test('deserialize state version 0', () => {
+    expect(state.deserialize(5, deserializeHelpers)).toEqual({
+      value: 5,
+      by: 1
+    })
+  })
+
+  test('deserialize state version 1', () => {
+    expect(
+      state.deserialize(
+        { __version__: 1, value: { value: 5 } },
+        deserializeHelpers
+      )
+    ).toEqual({ value: 5, by: 1 })
+  })
+
+  test('deserialize state version 2', () => {
+    expect(
+      state.deserialize(
+        { __version__: 2, value: { value: 5, by: 1 } },
+        deserializeHelpers
+      )
+    ).toEqual({ value: 5, by: 1 })
+  })
+
+  test('serialize', () => {
+    expect(state.serialize({ value: 3, by: -1 }, serializeHelpers)).toEqual({
+      __version__: 2,
+      value: { value: 3, by: -1 }
+    })
+  })
+
+  test('return type', () => {
+    const initial = { value: 3, by: -1 }
+    const value = state(initial, () => {})
+    expect(value.value()).toEqual(3)
+  })
+})
---
+++ packages/core/src/plugin-state/migration.ts
@@ -0,0 +1,156 @@
+import {
+  StateDescriptor,
+  StoreDeserializeHelpers,
+  StoreSerializeHelpers
+} from './types'
+
+export function migratable<S, T, R>(
+  type: StateDescriptor<S, T, R>
+): MigratableStateDescriptor<S, S, S, T, R> {
+  const state: AbstractMigratableStateDescriptor<S, S, S, T, R> = Object.assign(
+    function(
+      value: T,
+      onChange: (
+        updater: (oldValue: T, helpers: StoreDeserializeHelpers) => T
+      ) => void,
+      pluginProps?: unknown
+    ): R {
+      return type(value, onChange, pluginProps)
+    },
+    {
+      createInitialState(helpers: StoreDeserializeHelpers) {
+        return type.createInitialState(helpers)
+      },
+      deserialize(
+        serialized: S | Versionized<S>,
+        helpers: StoreDeserializeHelpers
+      ) {
+        if (isVersionized(serialized, 0)) {
+          return type.deserialize(serialized.value, helpers)
+        }
+        return type.deserialize(serialized, helpers)
+      },
+      serialize(
+        deserialized: T,
+        helpers: StoreSerializeHelpers
+      ): Versionized<S> {
+        return {
+          __version__: 0,
+          value: type.serialize(deserialized, helpers)
+        }
+      },
+      recursiveMigrate(previousState: S | Versionized<S>): S {
+        if (isVersionized<S>(previousState, 0)) {
+          return previousState.value
+        }
+        return previousState
+      }
+    }
+  )
+
+  return Object.assign(state, { migrate: nextMigrate })
+
+  function nextMigrate<S1, T1, R1>(
+    nextNextType: StateDescriptor<S1, T1, R1>,
+    f: (previousState: S) => S1
+  ) {
+    return migrate(state, nextNextType, 0, f)
+  }
+}
+
+function migrate<InitialS, AllS, S, T, R, S1, T1, R1>(
+  currentType: AbstractMigratableStateDescriptor<InitialS, AllS, S, T, R>,
+  nextType: StateDescriptor<S1, T1, R1>,
+  currentVersion: number,
+  f: (previousState: S) => S1
+): MigratableStateDescriptor<InitialS, AllS | S1, S1, T1, R1> {
+  const nextVersion = currentVersion + 1
+  const state: AbstractMigratableStateDescriptor<
+    InitialS,
+    AllS | S1,
+    S1,
+    T1,
+    R1
+  > = Object.assign(
+    function(
+      value: T1,
+      onChange: (
+        updater: (oldValue: T1, helpers: StoreDeserializeHelpers) => T1
+      ) => void,
+      pluginProps?: unknown
+    ): R1 {
+      return nextType(value, onChange, pluginProps)
+    },
+    {
+      createInitialState(helpers: StoreDeserializeHelpers) {
+        return nextType.createInitialState(helpers)
+      },
+      deserialize(
+        serialized: InitialS | Versionized<AllS | S1>,
+        helpers: StoreDeserializeHelpers
+      ): T1 {
+        if (isVersionized<S1>(serialized, nextVersion)) {
+          return nextType.deserialize(serialized.value, helpers)
+        }
+        const s = serialized as InitialS | Versionized<AllS>
+        return nextType.deserialize(f(currentType.recursiveMigrate(s)), helpers)
+      },
+      serialize(
+        deserialized: T1,
+        helpers: StoreSerializeHelpers
+      ): Versionized<S1> {
+        return {
+          __version__: nextVersion,
+          value: nextType.serialize(deserialized, helpers)
+        }
+      },
+      recursiveMigrate
+    }
+  )
+
+  return Object.assign(state, { migrate: nextMigrate })
+
+  function nextMigrate<S2, T2, R2>(
+    nextNextType: StateDescriptor<S2, T2, R2>,
+    f: (previousState: S1) => S2
+  ) {
+    return migrate(state, nextNextType, nextVersion, f)
+  }
+
+  function recursiveMigrate(
+    previousState: InitialS | Versionized<AllS | S1>
+  ): S1 {
+    if (isVersionized<S1>(previousState, nextVersion)) {
+      return previousState.value
+    }
+    return f(
+      currentType.recursiveMigrate(previousState as
+        | InitialS
+        | Versionized<AllS>)
+    )
+  }
+}
+
+function isVersionized<S>(
+  state: unknown,
+  version: number
+): state is Versionized<S> {
+  return (state as Versionized<S>).__version__ === version
+}
+
+interface Versionized<S> {
+  __version__: number
+  value: S
+}
+interface AbstractMigratableStateDescriptor<InitialS, AllS, S, T, R>
+  extends StateDescriptor<InitialS | Versionized<AllS>, T, R> {
+  recursiveMigrate: (previousState: InitialS | Versionized<AllS>) => S
+}
+
+interface MigratableStateDescriptor<InitialS, AllS, S, T, R>
+  extends AbstractMigratableStateDescriptor<InitialS, AllS, S, T, R> {
+  migrate<S1, T1, R1>(
+    nextType: StateDescriptor<S1, T1, R1>,
+    migrate: (previousState: S) => S1
+  ): MigratableStateDescriptor<InitialS, AllS | S1, S1, T1, R1>
+}
