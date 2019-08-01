322783f3818b836020ccc96ee8d65ba47d664aec
metzgaria <mele-metzger@web.de>
2019-08-01 18:03:21 +0200

    placeholder for anchor plugin

--- packages/plugin-anchor/src/editor.tsx
+++ packages/plugin-anchor/src/editor.tsx
@@ -17,6 +17,7 @@ export const AnchorEditor = (
       {focused ? (
         <EditorInput
           label="Identifier:"
+          placeholder="Gib hier einen Namen ein."
           value={state.value}
           onChange={e => {
             state.set(e.target.value)
