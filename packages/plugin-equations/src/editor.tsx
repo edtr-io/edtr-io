4ea93ec89ee775296c79326d535da6cc58dcad67
metzgaria <mele-metzger@web.de>
2019-08-15 10:05:01 +0200

    for now: always shows 1/3 width on big devices and 1 per line on small screens

--- packages/plugin-equations/src/editor.tsx
+++ packages/plugin-equations/src/editor.tsx
@@ -7,10 +7,11 @@ import { EquationsRenderer } from './renderer'
 import { equationsState } from '.'
 
 const DraggableContainer = styled.div({
-  border: '1px solid #000',
-  margin: '10px',
-  background: 'lightgrey',
-  padding: '10px'
+  border: '2px solid  #007ec1',
+  borderRadius: '5px',
+  margin: '5px 0',
+  padding: '3px',
+  background: 'transparent'
 })
 
 const RemoveButton = styled.button({
@@ -21,9 +22,9 @@ const RemoveButton = styled.button({
   border: 'none',
   float: 'right',
   background: 'transparent',
-  position: 'relative',
-  top: '-15px',
-  right: '-30px'
+  position: 'absolute',
+  top: '-5px',
+  right: '-5px'
 })
 
 const ColWrapper = styled.div({
@@ -32,12 +33,13 @@ const ColWrapper = styled.div({
 })
 
 const AddButton = styled.button({
-  borderRadius: '50%',
+  border: '2px solid  #007ec1',
+  borderRadius: '5px',
+  color: '#007ec1',
   outline: 'none',
-  width: '35px',
-  height: '35px',
-  border: 'none',
-  margin: 'auto'
+  padding: '5px',
+  margin: 'auto',
+  backgroundColor: 'transparent'
 })
 
 const AddButtonWrapper = styled.div({
@@ -56,81 +58,80 @@ export class EquationsEditor extends React.Component<
     state.steps.remove(index)
   }
   public render() {
-    const { editable, state } = this.props
-    if (editable) {
-      return (
-        <React.Fragment>
-          {/* eslint-disable @typescript-eslint/no-explicit-any */}
-          <DragDropContext
-            onDragEnd={result => {
-              const { source, destination } = result
-              if (!destination) {
-                return
-              }
-              state.steps.move(source.index, destination.index)
-            }}
-          >
-            <Droppable droppableId="default" direction="vertical">
-              {(provided: any) => {
-                return (
-                  <div ref={provided.innerRef} {...provided.droppableProps}>
-                    {state.steps().map((step, index) => {
-                      return (
-                        <Draggable
-                          key={index}
-                          draggableId={step.left.id}
-                          index={index}
-                        >
-                          {(provided: any) => {
-                            return (
-                              <DraggableContainer
-                                className="row"
-                                ref={provided.innerRef}
-                                {...provided.draggableProps}
-                                {...provided.dragHandleProps}
-                              >
-                                <div className="col-xs-12">
-                                  <RemoveButton
-                                    onClick={this.removeButton(index)}
-                                  >
-                                    <Icon icon={faTimes} />
-                                  </RemoveButton>
-                                </div>
-                                <div className="col-xs-4">
-                                  <strong>Left Side</strong>
-                                  <ColWrapper>{step.left.render()}</ColWrapper>
-                                </div>
-                                <div className="col-xs-4">
-                                  <strong>Right Side</strong>
-                                  <ColWrapper>{step.right.render()}</ColWrapper>
-                                </div>
-                                <div className="col-xs-4">
-                                  <strong>Transformation</strong>
-                                  <ColWrapper>
-                                    {step.transform.render()}
-                                  </ColWrapper>
-                                </div>
-                              </DraggableContainer>
-                            )
-                          }}
-                        </Draggable>
-                      )
-                    })}
-                  </div>
-                )
-              }}
-            </Droppable>
-          </DragDropContext>
-          {/* eslint-enable @typescript-eslint/no-explicit-any */}
-          <AddButtonWrapper>
-            <AddButton onClick={this.addButton}>
-              <Icon icon={faPlus} />
-            </AddButton>
-          </AddButtonWrapper>
-        </React.Fragment>
-      )
-    }
+    const { focused, state } = this.props
 
-    return <EquationsRenderer {...this.props} />
+    return focused ? (
+      <React.Fragment>
+        {/* eslint-disable @typescript-eslint/no-explicit-any */}
+        <DragDropContext
+          onDragEnd={result => {
+            const { source, destination } = result
+            if (!destination) {
+              return
+            }
+            state.steps.move(source.index, destination.index)
+          }}
+        >
+          <Droppable droppableId="default" direction="vertical">
+            {(provided: any) => {
+              return (
+                <div ref={provided.innerRef} {...provided.droppableProps}>
+                  {state.steps().map((step, index) => {
+                    return (
+                      <Draggable
+                        key={index}
+                        draggableId={step.left.id}
+                        index={index}
+                      >
+                        {(provided: any) => {
+                          return (
+                            <DraggableContainer
+                              className="row"
+                              ref={provided.innerRef}
+                              {...provided.draggableProps}
+                              {...provided.dragHandleProps}
+                            >
+                              <div className="col-xs-12">
+                                <RemoveButton
+                                  onClick={this.removeButton(index)}
+                                >
+                                  <Icon icon={faTimes} />
+                                </RemoveButton>
+                              </div>
+                              <div className="col-xs-4">
+                                <strong>Linke Seite</strong>
+                                <ColWrapper>{step.left.render()}</ColWrapper>
+                              </div>
+                              <div className="col-xs-4">
+                                <strong>Rechte Seite</strong>
+                                <ColWrapper>{step.right.render()}</ColWrapper>
+                              </div>
+                              <div className="col-xs-4">
+                                <strong>Umformung</strong>
+                                <ColWrapper>
+                                  {step.transform.render()}
+                                </ColWrapper>
+                              </div>
+                            </DraggableContainer>
+                          )
+                        }}
+                      </Draggable>
+                    )
+                  })}
+                </div>
+              )
+            }}
+          </Droppable>
+        </DragDropContext>
+        {/* eslint-enable @typescript-eslint/no-explicit-any */}
+        <AddButtonWrapper>
+          <AddButton onClick={this.addButton}>
+            <Icon icon={faPlus} /> Rechenschritt hinzufügen
+          </AddButton>
+        </AddButtonWrapper>
+      </React.Fragment>
+    ) : (
+      <EquationsRenderer {...this.props} />
+    )
   }
 }
