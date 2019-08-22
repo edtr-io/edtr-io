8c0f683e3ec16b10ab15ab8eeeddade2d4157737
Jonas Keinholz <git@inyono.dev>
2019-08-22 19:56:28 +0200

    plugin-input-exercise: migrate from legacy state

--- packages/plugin-input-exercise/src/index.ts
+++ packages/plugin-input-exercise/src/index.ts
@@ -10,10 +10,49 @@ export const answerObject = StateType.object({
   hasFeedback: StateType.boolean(),
   feedback: StateType.child()
 })
-export const inputExerciseState = StateType.object({
-  type: StateType.string('Text'),
-  answers: StateType.list(answerObject)
-})
+
+export const inputExerciseState = StateType.migratable(
+  StateType.object({
+    type: StateType.string('Text'),
+    correctAnswers: StateType.list(StateType.string('')),
+    wrongAnswers: StateType.list(
+      StateType.object({
+        value: StateType.string(''),
+        feedback: StateType.child()
+      })
+    )
+  })
+).migrate(
+  StateType.object({
+    type: StateType.string('Text'),
+    answers: StateType.list(answerObject)
+  }),
+  previousState => {
+    const answers = [
+      ...previousState.correctAnswers.map(value => {
+        return {
+          value,
+          isCorrect: true,
+          hasFeedback: false,
+          feedback: { plugin: 'text' }
+        }
+      }),
+      ...previousState.wrongAnswers.map(answer => {
+        return {
+          value: answer.value,
+          isCorrect: false,
+          hasFeedback: true,
+          feedback: answer.feedback
+        }
+      })
+    ]
+    return {
+      type: previousState.type,
+      answers
+    }
+  }
+)
+
 export const inputExercisePlugin: StatefulPlugin<typeof inputExerciseState> = {
   Component: InputExerciseEditor,
   state: inputExerciseState,
