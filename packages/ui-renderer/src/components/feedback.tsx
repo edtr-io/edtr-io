b0cc3ae2a2290e41360659eca1bcb2927774dbe7
Jonas Keinholz <git@inyono.dev>
2019-08-22 19:42:49 +0200

    merge conflicts

--- packages/ui-renderer/src/components/feedback.tsx
+++ packages/ui-renderer/src/components/feedback.tsx
@@ -1,6 +1,18 @@
 import * as React from 'react'
 
-import { styled } from '../theme'
+import { styled, createRendererUiTheme, RendererThemeProps } from '../theme'
+
+export const createFeedbackTheme = createRendererUiTheme<FeedbackTheme>(
+  theme => {
+    return {
+      color: theme.success.color,
+      successColor: theme.success.background,
+      failureColor: theme.warning.background,
+      borderSuccessColor: 'green',
+      borderFailureColor: '#faebcc'
+    }
+  }
+)
 
 export class Feedback extends React.Component<FeedbackProps> {
   public render() {
@@ -14,27 +26,53 @@ export class Feedback extends React.Component<FeedbackProps> {
     )
   }
 
-  private BoxFreeContainer = styled.div<{
-    correct?: boolean
-    showOnLeft?: boolean
-  }>(({ correct, showOnLeft }) => {
+  private BoxFreeContainer = styled.div<
+    {
+      correct?: boolean
+      showOnLeft?: boolean
+    } & RendererThemeProps
+  >(({ correct, showOnLeft, theme }) => {
+    const { successColor, failureColor } = createFeedbackTheme(
+      'feedback',
+      theme
+    )
     return {
-      color: correct ? '#95bc1a' : '#f7b07c',
+      color: correct ? successColor : failureColor,
       fontWeight: 'bold',
       textAlign: showOnLeft ? 'left' : 'right'
     }
   })
 
-  private BoxContainer = styled.div<{ correct?: boolean }>({
-    backgroundColor: '#fcf8e3',
-    borderColor: '#faebcc',
-    color: '#8a6d3b',
-    padding: '15px'
-  })
+  private BoxContainer = styled.div<{ correct?: boolean } & RendererThemeProps>(
+    ({ correct, theme }) => {
+      const {
+        successColor,
+        failureColor,
+        color,
+        borderSuccessColor,
+        borderFailureColor
+      } = createFeedbackTheme('feedback', theme)
+
+      return {
+        backgroundColor: correct ? successColor : failureColor,
+        borderColor: correct ? borderSuccessColor : borderFailureColor,
+        color,
+        padding: '15px'
+      }
+    }
+  )
 }
 
 export interface FeedbackProps {
   boxFree?: boolean
   isTrueAnswer?: boolean
   showOnLeft?: boolean
+}
+
+export interface FeedbackTheme {
+  color: string
+  successColor: string
+  failureColor: string
+  borderSuccessColor: string
+  borderFailureColor: string
 }
