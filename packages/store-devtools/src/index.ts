2cd4c12d75723aea640e81437c67134056a8e666
David Li <dawei.li@gmx.net>
2019-09-05 23:40:24 +0200

    remove immutable state invariant middleware

--- packages/store-devtools/src/index.ts
+++ packages/store-devtools/src/index.ts
@@ -1,11 +1,7 @@
-import { applyMiddleware, StoreEnhancer } from 'redux'
-import createImmutableStateInvariantMiddleware from 'redux-immutable-state-invariant'
+import { StoreEnhancer } from 'redux'
 import { composeWithDevTools } from 'remote-redux-devtools'
 
 export function createStoreDevtoolsEnhancer(defaultEnhancer: StoreEnhancer) {
   const composeEnhancers = composeWithDevTools({ realtime: true })
-  return composeEnhancers(
-    defaultEnhancer,
-    applyMiddleware(createImmutableStateInvariantMiddleware())
-  )
+  return composeEnhancers(defaultEnhancer)
 }
