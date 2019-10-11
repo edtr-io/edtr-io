109c92224eef3c3d0e145d9f71f378da98a72ac8
Stephan Kulla <git.mail@kulla.me>
2019-10-11 17:33:07 +0200

    jsx-converter: Format __tests__/index.ts

--- packages/jsx-converter/__tests__/index.ts
+++ packages/jsx-converter/__tests__/index.ts
@@ -7,23 +7,44 @@ test('toJSX', () => {
 })
 
 test('Function convertPluginStateToHast()', () => {
-  // Plugins with state of primitive data type
-  expect(convertPluginStateToHast({ plugin: 'hello', state: 42 })).toEqual(
-    hast('hello', '42')
+  const testCases = [
+    // Plugins with state of primitive data type
+    {
+      arg: { plugin: 'hello', state: 42 },
+      result: hast('hello', '42')
+    },
+    {
+      arg: { plugin: 'my-plugin', state: false },
+      result: hast('my-plugin', 'false')
+    },
+    {
+      arg: { plugin: 'stringplugin', state: 'I am a string' },
+      result: hast('stringplugin', 'I am a string')
+    }
+  ]
+
+  testCases.forEach(testCase =>
+    expect(convertPluginStateToHast(testCase.arg)).toEqual(testCase.result)
   )
-  expect(convertPluginStateToHast({ plugin: 'hello', state: false })).toEqual(
-    hast('hello', 'false')
-  )
-  expect(
-    convertPluginStateToHast({ plugin: 'hello', state: 'I am a string' })
-  ).toEqual(hast('hello', 'I am a string'))
 })
 
 test('Function convertValueToHast()', () => {
-  expect(convertValueToHast('foo', 42)).toEqual(hast('foo', '42'))
-  expect(convertValueToHast('foo', 'hello world')).toEqual(
-    hast('foo', 'hello world')
+  const testCases = [
+    {
+      args: ['foo', 42],
+      result: hast('foo', '42')
+    },
+    {
+      args: ['boolean', true],
+      result: hast('boolean', 'true')
+    },
+    {
+      args: ['string-plugin', 'hello world'],
+      result: hast('string-plugin', 'hello world')
+    }
+  ]
+
+  testCases.forEach(testCase =>
+    expect(convertValueToHast(...testCase.args)).toEqual(testCase.result)
   )
-  expect(convertValueToHast('foo', true)).toEqual(hast('foo', 'true'))
-  expect(convertValueToHast('foo', false)).toEqual(hast('foo', 'false'))
 })
