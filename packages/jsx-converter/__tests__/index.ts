aac59c9a70e76eecd658a96b49ca0989ad7e67fe
Stephan Kulla <git.mail@kulla.me>
2019-10-11 13:25:50 +0200

    jsx-converter: Add convertPropertyToHast

--- packages/jsx-converter/__tests__/index.ts
+++ packages/jsx-converter/__tests__/index.ts
@@ -1,5 +1,16 @@
-import { toJSX } from "../src";
+import hast from 'hastscript'
+
+import { toJSX, convertPropertyToHast } from '../src'
 
 test('toJSX', () => {
-  expect(toJSX({ plugin: "Foo", state: null})).toEqual("");
+  expect(toJSX({ plugin: 'Foo', state: null })).toEqual('')
+})
+
+test('Function convertPropertyToHast()', () => {
+  expect(convertPropertyToHast('foo', 42)).toEqual(hast('foo', '42'))
+  expect(convertPropertyToHast('foo', 'hello world')).toEqual(
+    hast('foo', 'hello world')
+  )
+  expect(convertPropertyToHast('foo', true)).toEqual(hast('foo', 'true'))
+  expect(convertPropertyToHast('foo', false)).toEqual(hast('foo', 'false'))
 })
--- packages/jsx-converter/src/index.ts
+++ packages/jsx-converter/src/index.ts
@@ -1,10 +1,54 @@
-import { EditorProps } from "@edtr-io/core"
+import { EditorProps } from '@edtr-io/core'
 
 /**
  * Converts an editor state into a JSX formatted string.
  *
  * @param state the editor state
  */
-export function toJSX(state: EditorProps["initialState"]): string {
-  return "";
+export function toJSX(state: EditorProps['initialState']): string {
+  return ''
+}
+
+/**
+ * Converts an object's property to an hast node. This function is used by
+ * `convertPluginStateToHast()` in order to convert the properties of an object.
+ *
+ * @param property The key of the object's property
+ * @param value The value of the object's property
+ */
+// TODO: Use type definitions for hast
+export function convertPropertyToHast(
+  property: string,
+  value: string | number | boolean
+): object {
+  return hastElement(property, undefined, [hastText(String(value))])
+}
+
+/**
+ * Creates an hast element with the tag name `tagName` and the given properties
+ * and children.
+ *
+ * @param tagName The tag name of the hast element
+ * @param properties Properties of the hast element
+ * @param children children of the hast element
+ */
+function hastElement(tagName: string, properties = {}, children = []) {
+  return {
+    type: 'element',
+    tagName: tagName,
+    properties: properties,
+    children: children
+  }
+}
+
+/**
+ * Converts the string `text` to an hast element.
+ *
+ * @param text String which shall be converted to an hast element
+ */
+function hastText(text: string) {
+  return {
+    type: 'text',
+    value: text
+  }
 }
