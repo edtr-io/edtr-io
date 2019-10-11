4de9a786ca95279c808c8191b0b1ed79cb63b172
metzgaria <mele-metzger@web.de>
2019-10-11 09:36:29 +0200

    Video URL can also be changed in plugin settings

--- packages/plugin-video/src/editor.tsx
+++ packages/plugin-video/src/editor.tsx
@@ -6,12 +6,27 @@ import { videoState } from '.'
 import { VideoRenderer } from './renderer'
 
 export const VideoEditor = (
-  props: StatefulPluginEditorProps<typeof videoState>
+  props: StatefulPluginEditorProps<typeof videoState> & {
+    renderIntoExtendedSettings?: (children: React.ReactNode) => React.ReactNode
+  }
 ) => {
   const { editable, focused, state } = props
   return (
     <React.Fragment>
       <VideoRenderer {...props} disableCursorEvents={editable} />
+      {props.renderIntoExtendedSettings
+        ? props.renderIntoExtendedSettings(
+            <EditorInput
+              label="Video URL:"
+              value={state.value}
+              onChange={e => {
+                state.set(e.target.value)
+              }}
+              textfieldWidth="80%"
+              editorInputWidth="100%"
+            />
+          )
+        : null}
       {focused ? (
         <PrimarySettings>
           <EditorInput
