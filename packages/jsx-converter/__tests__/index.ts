00d089850b79114038cf7ce29e09a46b7fdc71f3
Stephan Kulla <git.mail@kulla.me>
2019-10-15 20:39:10 +0200

    jsx-converter: Use test.each() in tests

--- packages/jsx-converter/__tests__/index.ts
+++ packages/jsx-converter/__tests__/index.ts
@@ -8,70 +8,45 @@ import {
 // Examples for serialization of plugin states
 const serializePluginExamples = [
   // Stateful plugins with state of primitive data type
-  {
-    plugin: { plugin: 'NumberStatePlugin', state: 42 },
-    markup: '<NumberStatePlugin>42</NumberStatePlugin>'
-  },
-  {
-    plugin: { plugin: 'boolean', state: false },
-    markup: '<boolean>false</boolean>'
-  },
-  {
-    plugin: { plugin: 'string-plugin', state: 'Hello World' },
-    markup: '<string-plugin>Hello World</string-plugin>'
-  }
+  [
+    { plugin: 'NumberStatePlugin', state: 42 },
+    '<NumberStatePlugin>42</NumberStatePlugin>'
+  ],
+  [{ plugin: 'boolean', state: false }, '<boolean>false</boolean>'],
+  [
+    { plugin: 'string-plugin', state: 'Hello World' },
+    '<string-plugin>Hello World</string-plugin>'
+  ]
 ]
 
-test('serializePlugin()', () => {
-  serializePluginExamples.forEach(example =>
-    expect(serializePlugin(example.plugin)).toEqual(example.markup)
+describe('serializePlugin()', () => {
+  test.each(serializePluginExamples)('Serialize plugin %j', (plugin, markup) =>
+    expect(serializePlugin(plugin)).toEqual(markup)
   )
 })
 
-test('convertPluginToHast()', () => {
-  const testCases = [
-    // Plugins with state of primitive data type
-    {
-      arg: { plugin: 'hello', state: 42 },
-      result: hast('hello', '42')
-    },
-    {
-      arg: { plugin: 'my-plugin', state: false },
-      result: hast('my-plugin', 'false')
-    },
-    {
-      arg: { plugin: 'stringplugin', state: 'I am a string' },
-      result: hast('stringplugin', 'I am a string')
-    }
-  ]
-
-  testCases.forEach(testCase =>
-    expect(convertPluginToHast(testCase.arg)).toEqual(testCase.result)
+describe('convertPluginToHast()', () => {
+  test.each([
+    [{ plugin: 'hello', state: 42 }, hast('hello', '42')],
+    [{ plugin: 'my-plugin', state: false }, hast('my-plugin', 'false')],
+    [
+      { plugin: 'string', state: 'string state' },
+      hast('string', 'string state')
+    ]
+  ])('Convert plugin %j to hast', (plugin, result) =>
+    expect(convertPluginToHast(plugin)).toEqual(result)
   )
 })
 
-type ConvertValueToHastExamples = {
-  args: [string, string | number | boolean]
-  result: any
-}[]
-
-test('convertValueToHast()', () => {
-  const testCases: ConvertValueToHastExamples = [
-    {
-      args: ['foo', 42],
-      result: hast('foo', '42')
-    },
-    {
-      args: ['boolean', true],
-      result: hast('boolean', 'true')
-    },
-    {
-      args: ['string-plugin', 'hello world'],
-      result: hast('string-plugin', 'hello world')
-    }
-  ]
-
-  testCases.forEach(testCase =>
-    expect(convertValueToHast(...testCase.args)).toEqual(testCase.result)
+describe('convertValueToHast()', () => {
+  test.each`
+    tagName         | value            | result
+    ${'foo'}        | ${42}            | ${hast('foo', '42')}
+    ${'boolean'}    | ${true}          | ${hast('boolean', 'true')}
+    ${'str-plugin'} | ${'hello world'} | ${hast('str-plugin', 'hello world')}
+  `(
+    'Convert value "$value" with tag name "$tagName"',
+    ({ tagName, value, result }) =>
+      expect(convertValueToHast(tagName, value)).toEqual(result)
   )
 })
