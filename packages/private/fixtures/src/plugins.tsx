00b04ad33f95127f426dbb318d0bec443df27fa4
Jonas Keinholz <git@inyono.dev>
2019-10-28 08:02:16 +0100

    fixture: use plugin factories

--- packages/private/fixtures/src/plugins.tsx
+++ packages/private/fixtures/src/plugins.tsx
@@ -1,22 +1,96 @@
 // Declaring these as devDependencies would lead to a dependency cycles. Since we only use these for testing anyways, we can safely disable the ESLint rule here
 /* eslint-disable import/no-extraneous-dependencies */
-import { child, list, number, object } from '@edtr-io/plugin'
-import { anchorPlugin } from '@edtr-io/plugin-anchor'
-import { blockquotePlugin } from '@edtr-io/plugin-blockquote'
-import { geogebraPlugin } from '@edtr-io/plugin-geogebra'
-import { highlightPlugin } from '@edtr-io/plugin-highlight'
-import { hintPlugin } from '@edtr-io/plugin-hint'
+import { child, list, number, object, Plugin } from '@edtr-io/plugin'
+import { createAnchorPlugin } from '@edtr-io/plugin-anchor'
+import { createBlockquotePlugin } from '@edtr-io/plugin-blockquote'
+import { createGeogebraPlugin } from '@edtr-io/plugin-geogebra'
+import { createHighlightPlugin } from '@edtr-io/plugin-highlight'
+import { createHintPlugin } from '@edtr-io/plugin-hint'
 import { createImagePlugin } from '@edtr-io/plugin-image'
-import { importantStatementPlugin } from '@edtr-io/plugin-important-statement'
-import { inputExercisePlugin } from '@edtr-io/plugin-input-exercise'
-import { rowsPlugin } from '@edtr-io/plugin-rows'
-import { scMcExercisePlugin } from '@edtr-io/plugin-sc-mc-exercise'
-import { serloInjectionPlugin } from '@edtr-io/plugin-serlo-injection'
-import { solutionPlugin } from '@edtr-io/plugin-solution'
-import { spoilerPlugin } from '@edtr-io/plugin-spoiler'
-import { tablePlugin } from '@edtr-io/plugin-table'
-import { textPlugin } from '@edtr-io/plugin-text'
-import { videoPlugin } from '@edtr-io/plugin-video'
+import { createImportantStatementPlugin } from '@edtr-io/plugin-important-statement'
+import { createInputExercisePlugin } from '@edtr-io/plugin-input-exercise'
+import { createRowsPlugin } from '@edtr-io/plugin-rows'
+import { createScMcExercisePlugin } from '@edtr-io/plugin-sc-mc-exercise'
+import { createSerloInjectionPlugin } from '@edtr-io/plugin-serlo-injection'
+import { createSolutionPlugin } from '@edtr-io/plugin-solution'
+import { createSpoilerPlugin } from '@edtr-io/plugin-spoiler'
+import { createTablePlugin } from '@edtr-io/plugin-table'
+import { createTextPlugin } from '@edtr-io/plugin-text'
+import { createVideoPlugin } from '@edtr-io/plugin-video'
+import { createEquationsPlugin } from '@edtr-io/plugin-equations'
+
+const registry = [
+  // Must be placed before files for onPaste
+  {
+    name: 'image',
+    ...createImagePlugin({
+      upload: () => Promise.resolve('foo'),
+      validate: () => {
+        return { valid: true }
+      }
+    })
+  },
+  {
+    name: 'anchor',
+    ...createAnchorPlugin()
+  },
+  {
+    name: 'blockquote',
+    ...createBlockquotePlugin()
+  },
+  {
+    name: 'equations',
+    ...createEquationsPlugin()
+  },
+  {
+    name: 'geogebra',
+    ...createGeogebraPlugin()
+  },
+  {
+    name: 'highlight',
+    ...createHighlightPlugin()
+  },
+  {
+    name: 'hint',
+    ...createHintPlugin()
+  },
+  {
+    name: 'inputExercise',
+    ...createInputExercisePlugin()
+  },
+  {
+    name: 'importantStatement',
+    ...createImportantStatementPlugin()
+  },
+  {
+    name: 'scMcExercise',
+    ...createScMcExercisePlugin()
+  },
+  {
+    name: 'serloInjection',
+    ...createSerloInjectionPlugin()
+  },
+  {
+    name: 'solution',
+    ...createSolutionPlugin()
+  },
+  {
+    name: 'spoiler',
+    ...createSpoilerPlugin()
+  },
+  {
+    name: 'table',
+    ...createTablePlugin()
+  },
+  {
+    name: 'video',
+    ...createVideoPlugin()
+  }
+]
+
+const textPlugin = createTextPlugin({
+  registry
+})
 
 const nestedArrayState = object({
   children: list(child('stateful'), 1)
@@ -28,42 +102,32 @@ const nestedState = object({
 
 const statefulState = number(0)
 
-export const plugins = {
-  anchor: anchorPlugin,
-  blockquote: blockquotePlugin,
-  geogebra: geogebraPlugin,
-  highlight: highlightPlugin,
-  hint: hintPlugin,
-  image: createImagePlugin({
-    upload: () => Promise.resolve('foo'),
-    validate: () => {
-      return { valid: true }
-    }
+export const plugins: Record<string, Plugin<any, any>> = {
+  rows: createRowsPlugin({
+    plugins: [{ name: 'text', ...textPlugin }, ...registry]
   }),
-  important: importantStatementPlugin,
-  injection: serloInjectionPlugin,
-  inputExercise: inputExercisePlugin,
-  rows: rowsPlugin,
-  scMcExercise: scMcExercisePlugin,
-  solution: solutionPlugin,
-  spoiler: spoilerPlugin,
-  table: tablePlugin,
   text: textPlugin,
-  video: videoPlugin,
-
   stateless: {
+    config: {},
     Component: () => null
   },
   stateful: {
+    config: {},
     Component: () => null,
     state: statefulState
   },
   nested: {
+    config: {},
     Component: () => null,
     state: nestedState
   },
   nestedArray: {
+    config: {},
     Component: () => null,
     state: nestedArrayState
   }
 }
+
+registry.forEach(({ name, ...plugin }) => {
+  plugins[name] = plugin
+})
