f63f2bb22efaca0574bb98789f41cd546eb9e88d
Stephan Kulla <git.mail@kulla.me>
2019-11-11 10:42:57 +0100

    jsx: Remove first attempt

--- packages/jsx-converter/__tests__/index.ts
+++ packages/jsx-converter/__tests__/index.ts
@@ -1,10 +1,3 @@
-import hast from 'hastscript'
-import {
-  serializePlugin,
-  convertPluginToHast,
-  convertValueToHast
-} from '../src'
-
 describe('Conversion of editor plugins to XML and back', () => {
   /*
    * ## Basics
@@ -362,50 +355,4 @@ describe('Conversion of editor plugins to XML and back', () => {
     test.todo('serialization')
     test.todo('deserialization')
   })
-})
-
-// Examples for serialization of plugin states
-const serializePluginExamples = [
-  // Stateful plugins with state of primitive data type
-  [
-    { plugin: 'NumberStatePlugin', state: 42 },
-    '<NumberStatePlugin>42</NumberStatePlugin>'
-  ],
-  [{ plugin: 'boolean', state: false }, '<boolean>false</boolean>'],
-  [
-    { plugin: 'string-plugin', state: 'Hello World' },
-    '<string-plugin>Hello World</string-plugin>'
-  ]
-]
-
-describe('serializePlugin()', () => {
-  test.each(serializePluginExamples)('Serialize plugin %j', (plugin, markup) =>
-    expect(serializePlugin(plugin)).toEqual(markup)
-  )
-})
-
-describe('convertPluginToHast()', () => {
-  test.each([
-    [{ plugin: 'hello', state: 42 }, hast('hello', '42')],
-    [{ plugin: 'my-plugin', state: false }, hast('my-plugin', 'false')],
-    [
-      { plugin: 'string', state: 'string state' },
-      hast('string', 'string state')
-    ]
-  ])('Convert plugin %j to hast', (plugin, result) =>
-    expect(convertPluginToHast(plugin)).toEqual(result)
-  )
-})
-
-describe('convertValueToHast()', () => {
-  test.each`
-    tagName         | value            | result
-    ${'foo'}        | ${42}            | ${hast('foo', '42')}
-    ${'boolean'}    | ${true}          | ${hast('boolean', 'true')}
-    ${'str-plugin'} | ${'hello world'} | ${hast('str-plugin', 'hello world')}
-  `(
-    'Convert value "$value" with tag name "$tagName"',
-    ({ tagName, value, result }) =>
-      expect(convertValueToHast(tagName, value)).toEqual(result)
-  )
 })
