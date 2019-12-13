b389e34f7351fb658f96cb652a581f525eb06d94
Wolfgang Rathgeb <W.Rathgeb@dropanote.de>
2019-12-13 12:30:48 +0100

    reorder import

--- packages/plugins/text/src/plugins/index.ts
+++ packages/plugins/text/src/plugins/index.ts
@@ -1,4 +1,5 @@
 import { TextPluginOptions } from '../factory/types'
+import { autoLink } from './auto-link'
 import { createColorPlugin } from './colors'
 import { createHeadingsPlugin } from './headings'
 import { createKatexPlugin } from './katex'
@@ -8,7 +9,6 @@ import { markdownShortcuts } from './markdown'
 import { createParagraphPlugin } from './paragraph'
 import { pluginSuggestions } from './plugin-suggestions'
 import { createRichTextPlugin } from './rich-text'
-import { autoLink } from './auto-link'
 
 export const plugins: TextPluginOptions['plugins'] = [
   pluginSuggestions,
