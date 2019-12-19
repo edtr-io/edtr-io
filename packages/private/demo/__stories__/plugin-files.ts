a03582ad41c7fb7cebb99ff1fc3e35e0161b25bd
Jonas Keinholz <git@inyono.dev>
2019-12-19 05:37:38 +0100

    demo: add missing plugins

---
+++ packages/private/demo/__stories__/plugin-files.ts
@@ -0,0 +1,12 @@
+import { addStory } from '../src'
+
+addStory('Plugins/Files/Initial State', {
+  state: {
+    plugin: 'rows',
+    state: [
+      {
+        plugin: 'files'
+      }
+    ]
+  }
+})
