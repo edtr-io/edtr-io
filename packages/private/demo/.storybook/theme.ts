e14345dc5273f54cb24589fca3dec29adf0c7ae5
Jonas Keinholz <git@inyono.dev>
2019-12-27 03:11:44 +0100

    demo: add ref to title

--- packages/private/demo/.storybook/theme.ts
+++ packages/private/demo/.storybook/theme.ts
@@ -1,9 +1,9 @@
 import { create } from '@storybook/theming'
 
+const title = process.env.TITLE ? ` (${process.env.TITLE})` : ''
+
 export const theme = create({
   base: 'light',
-  brandImage:
-    'https://github.com/edtr-io/edtr-io/blob/master/README_files/edtrio_full.svg',
-  brandTitle: 'Edtr.io',
+  brandTitle: `Edtr.io${title}`,
   brandUrl: 'https://edtr.io'
 })
