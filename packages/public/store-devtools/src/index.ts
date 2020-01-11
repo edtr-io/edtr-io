6062f1cb429f878d4d2f7d4117ace9d1cd0bd125
Jonas Keinholz <git@inyono.dev>
2020-01-11 06:30:01 +0100

    api: store-devtools

--- packages/public/store-devtools/src/index.ts
+++ packages/public/store-devtools/src/index.ts
@@ -1,7 +1,17 @@
 import { StoreEnhancer } from 'redux'
 import { composeWithDevTools } from 'remote-redux-devtools'
 
-export function createStoreDevtoolsEnhancer(defaultEnhancer: StoreEnhancer) {
+/**
+ * {@link @edtr-io/store#StoreEnhancerFactory | Enhances the store} with {@link https://github.com/zalmoxisus/remote-redux-devtools | Remote Redux DevTools}
+ *
+ * @param defaultEnhancer - The previous {@link redux#StoreEnhancer}
+ * @returns The {@link redux#StoreEnhancer} including Remote Redux DevTools
+ *
+ * @public
+ */
+export function createStoreDevtoolsEnhancer(
+  defaultEnhancer: StoreEnhancer
+): StoreEnhancer {
   const composeEnhancers = composeWithDevTools({ realtime: true })
   return composeEnhancers(defaultEnhancer)
 }
