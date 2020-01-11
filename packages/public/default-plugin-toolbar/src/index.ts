abbce084e16f6c447bb2a35c6f5338b1dffe243b
Jonas Keinholz <git@inyono.dev>
2020-01-11 09:42:10 +0100

    api: default-plugin-toolbar

--- packages/public/default-plugin-toolbar/src/index.ts
+++ packages/public/default-plugin-toolbar/src/index.ts
@@ -9,6 +9,13 @@ import { createOverlayTextarea } from './overlay-textarea'
 import { createPluginToolbarButton } from './plugin-toolbar-button'
 import { createPluginToolbarOverlayButton } from './plugin-toolbar-overlay-button'
 
+/**
+ * Creates the default {@link @edtr-io/plugin-toolbar#PluginToolbar | plugin toolbar}
+ *
+ * @param config - Optional configuration
+ * @returns The default {@link @edtr-io/plugin-toolbar#PluginToolbar | plugin toolbar}
+ * @public
+ */
 export function createDefaultPluginToolbar(
   config: DefaultPluginToolbarConfig = { primaryColor: 'rgb(70, 155, 255)' }
 ): PluginToolbar {
@@ -22,3 +29,5 @@ export function createDefaultPluginToolbar(
     PluginToolbarOverlayButton: createPluginToolbarOverlayButton(config)
   }
 }
+
+export { DefaultPluginToolbarConfig }
