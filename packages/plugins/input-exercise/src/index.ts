19b89220990170ee10cc01310353f55b7b944f06
Jonas Keinholz <git@inyono.dev>
2020-01-13 06:31:59 +0100

    api: plugin-input-exercise

--- packages/plugins/input-exercise/src/index.ts
+++ packages/plugins/input-exercise/src/index.ts
@@ -12,7 +12,8 @@ import {
 
 import { InputExerciseEditor } from './editor'
 
-function createInputExerciseState(feedback: Parameters<typeof child>) {
+/** @public */
+export function createInputExerciseState(feedback: Parameters<typeof child>) {
   const stateV0 = object({
     type: string('Text'),
     correctAnswers: list(string('')),
@@ -69,15 +70,19 @@ function createInputExerciseState(feedback: Parameters<typeof child>) {
       }
     })
 }
+/** @public */
 export type InputExerciseState = ReturnType<typeof createInputExerciseState>
+/** @public */
 export interface InputExerciseConfig {
   theme: { borderColor: string; borderStyle: string }
 }
+/** @public */
 export type InputExerciseProps = EditorPluginProps<
   InputExerciseState,
   InputExerciseConfig
 >
 
+/** @public */
 export function createInputExercisePlugin({
   theme = {},
   feedback = []
