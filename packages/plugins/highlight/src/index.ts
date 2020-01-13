4703c348ab416eed6456974aa7b381cfa7b5b895
Jonas Keinholz <git@inyono.dev>
2020-01-13 06:24:20 +0100

    api: plugin-highlight

--- packages/plugins/highlight/src/index.ts
+++ packages/plugins/highlight/src/index.ts
@@ -10,17 +10,22 @@ import * as React from 'react'
 import { HighlightEditor } from './editor'
 import { HighlightRenderer, HighlightRendererProps } from './renderer'
 
-const highlightState = object({
+/** @public */
+export const highlightState = object({
   text: string(''),
   language: string('text'),
   lineNumbers: boolean(false)
 })
+/** @public */
 export type HighlightState = typeof highlightState
+/** @public */
 export interface HighlightConfig {
   Renderer: React.ComponentType<HighlightRendererProps>
 }
+/** @public */
 export type HighlightProps = EditorPluginProps<HighlightState, HighlightConfig>
 
+/** @public */
 export function createHighlightPlugin(
   config: HighlightConfig = {
     Renderer: HighlightRenderer
