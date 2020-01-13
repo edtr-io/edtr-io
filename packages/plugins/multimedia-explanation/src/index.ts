8edae5739f869d86127925ebb17f192bbebe67a9
Jonas Keinholz <git@inyono.dev>
2020-01-13 06:33:38 +0100

    api: plugin-multimedia-explanation

--- packages/plugins/multimedia-explanation/src/index.ts
+++ packages/plugins/multimedia-explanation/src/index.ts
@@ -9,7 +9,8 @@ import {
 
 import { MultimediaExplanationEditor } from './editor'
 
-const createMultimediaExplanationState = (
+/** @public */
+export const createMultimediaExplanationState = (
   config: MultimediaExplanationConfig
 ) =>
   object({
@@ -18,20 +19,24 @@ const createMultimediaExplanationState = (
     illustrating: boolean(true),
     width: number(50) // percent
   })
+/** @public */
 export type MultimediaExplanationState = ReturnType<
   typeof createMultimediaExplanationState
 >
+/** @public */
 export interface MultimediaExplanationConfig {
   plugins: {
     name: string
     title: string
   }[]
 }
+/** @public */
 export type MultimediaExplanationProps = EditorPluginProps<
   MultimediaExplanationState,
   MultimediaExplanationConfig
 >
 
+/** @public */
 export const createMultimediaExplanationPlugin = (
   config: MultimediaExplanationConfig
 ): EditorPlugin<MultimediaExplanationState, MultimediaExplanationConfig> => {
