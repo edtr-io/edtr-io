f9c12d772a15e5ba4f6c3e6eefb120007790edba
Jonas Keinholz <git@inyono.dev>
2020-01-26 21:24:56 +0100

    plugin-input-exercise: remove migratable

--- packages/plugins/input-exercise/src/index.ts
+++ packages/plugins/input-exercise/src/index.ts
@@ -2,9 +2,7 @@ import {
   boolean,
   child,
   list,
-  migratable,
   object,
-  StateTypeSerializedType,
   string,
   EditorPluginProps,
   EditorPlugin
@@ -17,61 +15,17 @@ import { InputExerciseEditor } from './editor'
  * @public
  */
 export function createInputExerciseState(feedback: Parameters<typeof child>) {
-  const stateV0 = object({
-    type: string('Text'),
-    correctAnswers: list(string('')),
-    wrongAnswers: list(
-      object({
-        value: string(''),
-        feedback: child(...feedback)
-      })
-    )
-  })
   const answerObject = object({
     value: string(''),
     isCorrect: boolean(),
     feedback: child(...feedback)
   })
-  const stateV1 = object({
-    type: string('Text'),
-    answers: list(answerObject)
-  })
 
-  const stateV2 = object({
+  return object({
     type: string('input-string-normalized-match-challenge'),
     unit: string(''),
     answers: list(answerObject)
   })
-
-  return migratable(stateV0)
-    .migrate(stateV1, previousState => {
-      return {
-        type: previousState.type,
-        answers: [
-          ...previousState.correctAnswers.map(answer => {
-            return {
-              value: answer,
-              isCorrect: true,
-              feedback: { plugin: 'text' } as StateTypeSerializedType<
-                ReturnType<typeof child>
-              >
-            }
-          }),
-          ...previousState.wrongAnswers.map(answer => {
-            return {
-              ...answer,
-              isCorrect: false
-            }
-          })
-        ]
-      }
-    })
-    .migrate(stateV2, previousState => {
-      return {
-        ...previousState,
-        unit: ''
-      }
-    })
 }
 /** @public */
 export type InputExerciseState = ReturnType<typeof createInputExerciseState>
