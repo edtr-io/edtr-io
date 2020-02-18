23c68619862ba97cd5a29992d095cbe5edf794af
Jonas Keinholz <git@inyono.dev>
2020-02-18 07:34:18 +0100

    fix postdeploy script

--- packages/private/demo/__helpers__/deploy.ts
+++ packages/private/demo/__helpers__/deploy.ts
@@ -1,5 +1,5 @@
-import * as path from 'path'
 import { spawnSync } from 'child_process'
+import * as path from 'path'
 import * as request from 'request'
 
 const bucket = 'gs://storybook.edtr.io'
@@ -7,21 +7,22 @@ const bucket = 'gs://storybook.edtr.io'
 export async function exec({
   secret,
   commit,
-  ref: rawRef
-}: {
-  secret?: string
-  commit?: string
-  ref?: string
-}) {
+  ref: rawRef,
+  removePreviousCommit
+}: Partial<Payload> & { removePreviousCommit: boolean }) {
   if (!secret) throw new Error('secret not set')
   if (!commit) return
   if (!rawRef) return
   const ref = encodeURIComponent(rawRef)
 
   upload(commit)
-  const previousCommit = await getCommit(ref)
-  if (previousCommit) remove(previousCommit)
-  await setCommit(ref, commit)
+  const previousCommit = await getCommit({ ref, secret })
+  if (previousCommit && removePreviousCommit) remove(previousCommit)
+  await setCommit({
+    ref,
+    commit,
+    secret
+  })
   console.log(`Storybook published: https://storybook.edtr.io/${ref}/`)
 
   function upload(commit: string) {
@@ -43,40 +44,46 @@ export async function exec({
       stdio: 'inherit'
     })
   }
+}
 
-  function getCommit(ref: string) {
-    return new Promise<string | null>((resolve, reject) => {
-      request.get(
-        `https://api.cloudflare.com/client/v4/accounts/0e24f0f2e24dfe914ff15504899b0156/storage/kv/namespaces/f2d4ac58853245febc094ea6005a92e0/values/${ref}`,
-        {
-          headers: {
-            Authorization: `Bearer ${secret}`
-          }
-        },
-        (error, res) => {
-          if (error) return reject(error)
-          if (res.statusCode === 404) return resolve(null)
-          return resolve(res.body)
+export function getCommit({ secret, ref }: Pick<Payload, 'secret' | 'ref'>) {
+  return new Promise<string | null>((resolve, reject) => {
+    request.get(
+      `https://api.cloudflare.com/client/v4/accounts/0e24f0f2e24dfe914ff15504899b0156/storage/kv/namespaces/f2d4ac58853245febc094ea6005a92e0/values/${ref}`,
+      {
+        headers: {
+          Authorization: `Bearer ${secret}`
         }
-      )
-    })
-  }
+      },
+      (error, res) => {
+        if (error) return reject(error)
+        if (res.statusCode === 404) return resolve(null)
+        return resolve(res.body)
+      }
+    )
+  })
+}
 
-  function setCommit(ref: string, commit: string) {
-    return new Promise<void>((resolve, reject) => {
-      request.put(
-        `https://api.cloudflare.com/client/v4/accounts/0e24f0f2e24dfe914ff15504899b0156/storage/kv/namespaces/f2d4ac58853245febc094ea6005a92e0/values/${ref}`,
-        {
-          headers: {
-            Authorization: `Bearer ${secret}`
-          },
-          body: commit
+export function setCommit({ secret, commit, ref }: Payload) {
+  return new Promise<void>((resolve, reject) => {
+    request.put(
+      `https://api.cloudflare.com/client/v4/accounts/0e24f0f2e24dfe914ff15504899b0156/storage/kv/namespaces/f2d4ac58853245febc094ea6005a92e0/values/${ref}`,
+      {
+        headers: {
+          Authorization: `Bearer ${secret}`
         },
-        error => {
-          if (error) return reject(error)
-          return resolve()
-        }
-      )
-    })
-  }
+        body: commit
+      },
+      error => {
+        if (error) return reject(error)
+        return resolve()
+      }
+    )
+  })
+}
+
+export interface Payload {
+  secret: string
+  commit: string
+  ref: string
 }
--- scripts/deploy-demo.ts
+++ scripts/deploy-demo.ts
@@ -1,29 +1,29 @@
-import { exec } from '@edtr-io/internal__demo/__helpers__/deploy'
+import { exec, setCommit } from '@edtr-io/internal__demo/__helpers__/deploy'
 import { spawnSync } from 'child_process'
 
 /* eslint-disable @typescript-eslint/no-var-requires, import/no-commonjs */
 const { version } = require('../lerna.json')
 const secret = require('./cloudflare.secret.json')
 
-spawnSync(
-  'yarn',
-  ['lerna', 'run', '--scope=@edtr-io/internal__demo', 'build'],
-  {
-    stdio: 'inherit',
-    env: {
-      TITLE: version
-    }
-  }
-)
+const commit = getCommit()
 
 exec({
   secret,
-  commit: getCommit(),
-  ref: version
-}).catch(e => {
-  console.log('Failed', e)
-  process.exit(1)
+  commit,
+  ref: version,
+  removePreviousCommit: false
 })
+  .catch(e => {
+    console.log('Failed', e)
+    process.exit(1)
+  })
+  .then(() => {
+    return setCommit({
+      secret,
+      ref: 'latest',
+      commit
+    })
+  })
 
 function getCommit() {
   const result = spawnSync('git', ['rev-parse', 'HEAD'], {
