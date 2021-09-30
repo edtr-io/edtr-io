fdf78743b6d1f6e56ae76721d28b55ade053d313
Stephan Kulla <git.mail@kulla.me>
2021-09-30 15:56:13 +0200

    Refactor packages/plugins/input-exercise/src/editor.tsx

--- packages/plugins/input-exercise/src/editor.tsx
+++ packages/plugins/input-exercise/src/editor.tsx
@@ -41,75 +41,65 @@ export function InputExerciseEditor(props: InputExerciseProps) {
       props.state.answers.map((answer) => answer.feedback.id)
     )
   const [previewActive, setPreviewActive] = React.useState(false)
-  return (
+  return editable ? (
     <React.Fragment>
-      {editable ? (
-        <React.Fragment>
-          <PreviewOverlay focused={nestedFocus} onChange={setPreviewActive}>
-            <InputExerciseRenderer {...props} />
-          </PreviewOverlay>
-          {nestedFocus && !previewActive ? (
-            <React.Fragment>
-              {state.answers.map((answer, index: number) => {
-                return (
-                  <InteractiveAnswer
-                    i18n={props.config.i18n}
-                    key={answer.feedback.id}
-                    answer={
-                      <AnswerTextfield
-                        value={answer.value.value}
-                        placeholder={i18n.answer.value.placeholder}
-                        type="text"
-                        onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-                          answer.value.set(e.target.value)
-                        }}
-                      />
-                    }
-                    feedback={answer.feedback.render()}
-                    feedbackID={answer.feedback.id}
-                    isActive={answer.isCorrect.value}
-                    handleChange={() => {
-                      answer.isCorrect.set(!answer.isCorrect.value)
-                    }}
-                    remove={() => {
-                      state.answers.remove(index)
-                    }}
-                    focusedElement={focusedElement || undefined}
-                  />
-                )
-              })}
-              <AddButton onClick={() => state.answers.insert()}>
-                {i18n.answer.addLabel}
-              </AddButton>
-            </React.Fragment>
-          ) : null}
-          {props.renderIntoSettings(
-            <React.Fragment>
-              <OverlayInput
-                label={i18n.unit.label}
-                value={state.unit.value}
-                onChange={(e) => {
-                  state.unit.set(e.target.value)
-                }}
-              />
-              <OverlaySelect
-                label={i18n.type.label}
-                onChange={(event: React.ChangeEvent<HTMLSelectElement>) => {
-                  state.type.set(translateDataName(event.target.value))
-                }}
-                value={translateDataType(state.type.value as InputExerciseType)}
-                options={R.values(
-                  R.mapObjIndexed((type) => {
-                    return translateDataType(type)
-                  }, InputExerciseType)
-                )}
-              />
-            </React.Fragment>
-          )}
-        </React.Fragment>
-      ) : (
+      <PreviewOverlay focused={nestedFocus} onChange={setPreviewActive}>
         <InputExerciseRenderer {...props} />
+      </PreviewOverlay>
+      {nestedFocus && !previewActive && (
+        <React.Fragment>
+          {state.answers.map((answer, index: number) => {
+            return (
+              <InteractiveAnswer
+                i18n={props.config.i18n}
+                key={answer.feedback.id}
+                answer={
+                  <AnswerTextfield
+                    value={answer.value.value}
+                    placeholder={i18n.answer.value.placeholder}
+                    type="text"
+                    onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                      answer.value.set(e.target.value)
+                    }}
+                  />
+                }
+                feedback={answer.feedback.render()}
+                feedbackID={answer.feedback.id}
+                isActive={answer.isCorrect.value}
+                handleChange={() =>
+                  answer.isCorrect.set(!answer.isCorrect.value)
+                }
+                remove={() => state.answers.remove(index)}
+                focusedElement={focusedElement || undefined}
+              />
+            )
+          })}
+          <AddButton onClick={() => state.answers.insert()}>
+            {i18n.answer.addLabel}
+          </AddButton>
+        </React.Fragment>
+      )}
+      {props.renderIntoSettings(
+        <React.Fragment>
+          <OverlayInput
+            label={i18n.unit.label}
+            value={state.unit.value}
+            onChange={(e) => state.unit.set(e.target.value)}
+          />
+          <OverlaySelect
+            label={i18n.type.label}
+            onChange={(event: React.ChangeEvent<HTMLSelectElement>) => {
+              state.type.set(translateDataName(event.target.value))
+            }}
+            value={translateDataType(state.type.value as InputExerciseType)}
+            options={R.values(
+              R.mapObjIndexed(translateDataType, InputExerciseType)
+            )}
+          />
+        </React.Fragment>
       )}
     </React.Fragment>
+  ) : (
+    <InputExerciseRenderer {...props} />
   )
 }
