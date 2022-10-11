31a9684917c2320f30bbe0e27c9efb31c4eeff64
Jonas Keinholz <git@inyono.dev>
2022-10-11 11:51:28 +0200

    finish renderElement

--- packages/plugins/text/src/index.tsx
+++ packages/plugins/text/src/index.tsx
@@ -5,25 +5,36 @@ import {
   SerializedScalarStateType,
   StateTypeValueType,
 } from '@edtr-io/plugin'
-import { DeepPartial } from '@edtr-io/ui'
+import { DeepPartial, styled } from '@edtr-io/ui'
+import KaTeX from 'katex'
 import * as React from 'react'
 import { Descendant, Range, BaseEditor, createEditor } from 'slate'
 import { ReactEditor, Editable, withReact, Slate } from 'slate-react'
 
 // TODO: Move to a better place
 const emptyDocument: StateTypeValueType<TextPluginState> = {
-  value: [{ type: 'paragraph', children: [{ text: '' }] }],
+  value: [{ type: 'p', children: [{ text: '' }] }],
   selection: null,
 }
 
 // TODO: config of colours
 const blue = '#1794c1',
-    green = '#469a40',
-    orange = '#ff6703'
+  green = '#469a40',
+  orange = '#ff6703'
 
 const config = {
-  colors: [blue, green, orange]
+  colors: [blue, green, orange],
 }
+
+const KaTeXSpan = styled.span<{ element: MathElement }>(({ element }) => {
+  if (!element.inline) {
+    return {
+      display: 'block',
+      margin: '1em 0',
+      textAlign: 'center',
+    }
+  }
+})
 
 function TextEditor(props: TextProps) {
   const [editor] = React.useState(() => withReact(createEditor()))
@@ -33,14 +44,78 @@ function TextEditor(props: TextProps) {
     <Slate editor={editor} value={props.state.value.value}>
       <Editable
         renderElement={(props) => {
+          if (props.element.type === 'h') {
+            const level = props.element.level
+            const headingLevel = level <= 6 && level >= 1 ? level : 6
+            return React.createElement(
+              `h${headingLevel}`,
+              props.attributes,
+              <>{props.children}</>
+            )
+          }
+          if (props.element.type === 'a') {
+            return (
+              <a
+                href={props.element.href}
+                style={{ cursor: 'pointer' }}
+                {...props.attributes}
+              >
+                {props.children}
+              </a>
+            )
+          }
+
+          if (props.element.type === 'unordered-list') {
+            return <ul {...props.attributes}>{props.children}</ul>
+          }
+          if (props.element.type === 'ordered-list') {
+            return <ol {...props.attributes}>{props.children}</ol>
+          }
+          if (props.element.type === 'list-item') {
+            return <li {...props.attributes}>{props.children}</li>
+          }
+          if (props.element.type === 'list-item-child') {
+            return <div {...props.attributes}>{props.children}</div>
+          }
+
+          if (props.element.type === 'math') {
+            const { inline, src } = props.element
+            const html = KaTeX.renderToString(
+              `${inline ? '' : '\\displaystyle '}${src}`,
+              {
+                displayMode: false,
+                throwOnError: false,
+              }
+            )
+
+            return (
+              <KaTeXSpan
+                dangerouslySetInnerHTML={{ __html: html }}
+                element={props.element}
+              />
+            )
+          }
+
+          if (props.element.type != 'p') {
+            console.log(props.element)
+          }
           return <p {...props.attributes}>{props.children}</p>
         }}
         renderLeaf={(props) => {
           if (props.text.strong) {
             return <strong {...props.attributes}>{props.text.text}</strong>
           }
-          if (typeof props.text.color === "number") {
-            return <span style={{color: config.colors[props.text.color % config.colors.length]}} {...props.attributes}>{props.text.text}</span>
+          if (typeof props.text.color === 'number') {
+            return (
+              <span
+                style={{
+                  color: config.colors[props.text.color % config.colors.length],
+                }}
+                {...props.attributes}
+              >
+                {props.text.text}
+              </span>
+            )
           }
           if (props.text.code) {
             return <code {...props.attributes}>{props.text.text}</code>
@@ -214,10 +289,59 @@ export interface TextPluginConfig {
 export type TextProps = EditorPluginProps<TextPluginState, TextConfig>
 
 // TODO: We need to configure this!
-interface CustomElement {
-  type: 'paragraph'
+type CustomElement =
+  | Paragraph
+  | OrderedList
+  | UnorderedList
+  | ListItem
+  | ListItemText
+  | Heading
+  | Link
+  | MathElement
+
+interface Heading {
+  type: 'h'
+  level: 1 | 2 | 3 | 4 | 5 | 6
   children: CustomText[]
 }
+
+interface Paragraph {
+  type: 'p'
+  children: CustomText[]
+}
+
+interface Link {
+  type: 'a'
+  href: string
+  children: CustomText[]
+}
+
+interface UnorderedList {
+  type: 'unordered-list'
+  children: ListItem[]
+}
+
+interface OrderedList {
+  type: 'ordered-list'
+  children: ListItem[]
+}
+
+interface ListItem {
+  type: 'list-item'
+  children: ListItemText[]
+}
+
+interface ListItemText {
+  type: 'list-item-child'
+  children: CustomText[]
+}
+
+interface MathElement {
+  type: 'math'
+  src: string
+  inline: boolean
+}
+
 interface CustomText {
   text: string
   strong?: true
