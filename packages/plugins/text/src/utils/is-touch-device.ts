265baf702edf3eff1cfe51fc63588cf7b0e63c29
Vitomir Budimir <budimirvitomir@gmail.com>
2023-02-13 14:28:17 +0100

    refactor: explain is touch device util todo

--- packages/plugins/text/src/utils/is-touch-device.ts
+++ packages/plugins/text/src/utils/is-touch-device.ts
@@ -3,7 +3,7 @@ export function isTouchDevice() {
     typeof window !== 'undefined' &&
     ('ontouchstart' in window ||
       navigator.maxTouchPoints > 0 ||
-      // @ts-expect-error TODO:
+      // @ts-expect-error TODO: Remove this if IE10 should not be supported
       navigator.msMaxTouchPoints > 0)
   )
 }
