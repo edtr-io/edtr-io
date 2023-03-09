5b5888f59b7d6c558077abe581469698c19629d0
Stephan Kulla <git.mail@kulla.me>
2023-03-09 12:19:13 +0100

    fix(text-plugin): Remove "navigator.msMaxTouchPoints"

--- packages/plugins/text/src/utils/is-touch-device.ts
+++ packages/plugins/text/src/utils/is-touch-device.ts
@@ -1,9 +1,6 @@
 export function isTouchDevice() {
   return (
     typeof window !== 'undefined' &&
-    ('ontouchstart' in window ||
-      navigator.maxTouchPoints > 0 ||
-      // @ts-expect-error TODO: Remove this if IE10 should not be supported
-      navigator.msMaxTouchPoints > 0)
+    ('ontouchstart' in window || navigator.maxTouchPoints > 0)
   )
 }
