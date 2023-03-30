4a010ad62bb058440754d0d33094f9ef558b0cac
Entkenntnis <Entkenntnis@users.noreply.github.com>
2023-03-30 11:10:53 +0200

    come on, it has to work!!

--- packages/public/ui/src/index.ts
+++ packages/public/ui/src/index.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 /**
  * Provides utils for the User Interface
  *
@@ -5,7 +7,29 @@
  */
 import _styled from 'styled-components'
 
-const { defaultImport } = await import('default-import')
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const defaultImport = (mod: any) => {
+  if (typeof mod !== 'object' || mod === null) {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+    return mod
+  }
+  // Webpack provides a Module tag to match NodeJS' Module module
+  const defaultVal =
+    Symbol.toStringTag in mod && mod[Symbol.toStringTag] === 'Module'
+      ? mod.default ?? mod
+      : mod
+  if (
+    '__esModule' in defaultVal &&
+    // eslint-disable-next-line @typescript-eslint/naming-convention
+    defaultVal.__esModule &&
+    defaultVal.default !== undefined
+  ) {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+    return defaultVal.default
+  }
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+  return defaultVal
+}
 
 /**
  * Re-export of {@link https://styled-components.com/docs/api#primary | `styled` in `styled-components` }
